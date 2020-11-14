7923ea426b2785f0c8a846a31c3ab40896db1746
Sara Nolen <nolensara1@gmail.com>
2020-11-14 19:07:27 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+# create a method for number of turns that have been played
+def turn_count(board)
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  counter =  0
+  board.each do |token == "X" || token == "O"|
+  counter += 1
+
+end
+end
+
+
+# create a method for the  current player
+def current_player(board)
+
+end
